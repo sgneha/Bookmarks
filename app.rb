d8fc2ff082dafad4df73c14160a27c2e5541a3ba
sgneha <sgneha05@gmail.com>
2020-03-09 16:47:31 +0000

    hardcoded bookmarks as arrays

--- app.rb
+++ app.rb
@@ -3,8 +3,19 @@
 require 'sinatra/base'
 require './lib/bookmark'
 class Bookmark_manager < Sinatra::Base
+  get '/' do
+    'Bookmark Manager'
+  end
+
   get '/bookmarks' do
-    erb :bookmarks
+    bookmarks = [
+      'http://www.makersacademy.com',
+      'http://www.destroyallsoftware.com',
+      'http://www.google.com'
+    ]
+    bookmarks.join
+
+    erb :'bookmarks/index'
   end
 
   run! if app_file == $PROGRAM_NAME
--- spec/features/viewing_bookmark_spec.rb
+++ spec/features/viewing_bookmark_spec.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 feature 'View Bookmarks' do
-  scenario 'view the list' do
+  scenario 'view the bookmarks' do
     visit('/bookmarks')
-    expect(page).to have_content 'Bookmark Manager'
+    expect(page).to have_content 'http://www.makersacademy.com'
+    expect(page).to have_content 'http://www.destroyallsoftware.com'
+    expect(page).to have_content 'http://www.google.com'
   end
 end
