356bbe66e55b354616d37b4730186b08e29461b0
sgneha <sgneha05@gmail.com>
2020-03-11 17:25:29 +0000

    step 10 befor model

--- app.rb
+++ app.rb
@@ -10,7 +10,18 @@ class BookmarkManager < Sinatra::Base
 
   get '/bookmarks' do
     @bookmarks = Bookmark.all
-    erb :'bookmarks/index'
+    erb :"bookmarks/index"
+  end
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+  post '/bookmarks' do
+    p params
+    p 'form data submitted to the /bookmarks route!'
+    url = params['url']
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    redirect '/bookmarks'
   end
 
   run! if app_file == $PROGRAM_NAME
