7b62c2ad504adfc718c5480fb1e27a5fe0b44c99
sgneha <sgneha05@gmail.com>
2020-03-12 18:51:49 +0000

    step 11 completed

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class BookmarkManager < Sinatra::Base
     erb :"bookmarks/new"
   end
   post '/bookmarks' do
-    Bookmarks.create(url: params['url'])
+    Bookmark.create(url: params[:url], title: params[:title])
     redirect '/bookmarks'
   end
 
--- spec/features/viewing_bookmark_spec.rb
+++ spec/features/viewing_bookmark_spec.rb
@@ -16,13 +16,13 @@ feature 'View Bookmarks' do
     # connection.exec("INSERT INTO bookmarks VALUES(1,'http://www.makersacademy.com');")
     # connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
     # connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
-    Bookmark.create(url: 'http://www.makersacademy.com')
-    Bookmark.create(url: 'http://www.destroyallsoftware.com')
-    Bookmark.create(url: 'http://www.google.com')
+    Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    Bookmark.create(url: 'http://www.destroyallsoftware.com', title: 'Destroy All Software')
+    Bookmark.create(url: 'http://www.google.com', title: 'Google')
 
     visit('/bookmarks')
-    expect(page).to have_content 'http://www.makersacademy.com'
-    expect(page).to have_content 'http://www.destroyallsoftware.com'
-    expect(page).to have_content 'http://www.google.com'
+    expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
+    expect(page).to have_link('Destroy All Software',  href: 'http://www.destroyallsoftware.com')
+    expect(page).to have_link('Google', href: 'http://www.google.com')
   end
 end
