fae730ab15bca5ad5d5e9e5080a7730eda7c6274
sgneha <sgneha05@gmail.com>
2020-03-13 11:08:06 +0000

    step 12 delete feature in controller

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra/base'
 require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions, :method_override
   get '/' do
     'Bookmark Manager'
   end
@@ -17,6 +18,12 @@ class BookmarkManager < Sinatra::Base
   end
   post '/bookmarks' do
     Bookmark.create(url: params[:url], title: params[:title])
+    redirect '/bookmarks'
+  end
+  delete '/bookmarks/:id' do
+    p params
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("DELETE FROM bookmarks WHERE id =#{params['id']}")
     redirect '/bookmarks'
   end
 
---
+++ spec/features/deleting_a_bookmark_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+feature 'Deletin g a bookmark' do
+  scenario 'A user can delete a bookmark' do
+    Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    visit('/bookmarks')
+    expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
+    first('.bookmark').click_button 'Delete'
+    expect(current_path).to eq '/bookmarks'
+    expect(page).not_to have_link('Makers Academy', href: 'http://www.makersacademy.com')
+  end
+end
